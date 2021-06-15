4360ca9c588161c7d5ef9e3ad35820f631cd0db7
Eduardo Villalobo <eduardovillalobo23@gmai.com>
2021-06-15 18:36:31 -0300

    modelo de vacantes y vista

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-const path = require('path')
-const router = require('./routes')
+const mongoose = require('mongoose');
+require('./config/db');
+
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const router = require('./routes');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')
+
+require('dotenv').config({path: 'variables.env'});
 const app = express()
 
 app.engine('handlebars',
@@ -11,6 +19,15 @@ app.engine('handlebars',
 
 app.set('view engine', 'handlebars')
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(cookieParser())
+app.use(session({
+    secret: process.env.SECRETO,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({mongoUrl:process.env.DATABASE})
+}))
 app.use('/', router())
 
-app.listen(5000)
+
+app.listen(process.env.PUERTO)
